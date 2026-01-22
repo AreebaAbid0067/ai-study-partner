4e80524cf0d52c9a26e2a1df9abdb13829967a62
AreebaAbid0067 <aareeba006@gmail.com>
2026-01-22 22:34:29 +0400

    'upload document' code written

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from routes import documents
 
 app = FastAPI(title="AI Study Partner API")
 
@@ -10,6 +11,9 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+# Register routes
+app.include_router(documents.router)
 
 @app.get("/checkin")
 async def health_check():
--- backend/routes/documents.py
+++ backend/routes/documents.py
@@ -1,5 +1,20 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import APIRouter, File, UploadFile, HTTPException
 from typing import Annotated
 
+router = APIRouter(prefix="/documents", tags=["documents"])
 
-app = FastAPI()
+@router.post("/upload")
+async def upload_file(file: Annotated[UploadFile, File()]):
+    if file.content_type not in ["application/pdf", "text/plain", "application/msword"]:
+        raise HTTPException(status_code=400, detail="Invalid file type")
+        # this is also an exception so if u put try catch...the exception 
+        # block will take it as exception and instead of displaying this message it will 
+        # display the message in the exception block u wrote
+    
+    return {"filename": file.filename,
+            "message":"file uploaded successfully"}
+
+
+
+
+
